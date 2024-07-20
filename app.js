5f6e81f9eb8d107acfa955af87809333402d012a
Beingrahuul <beingrahuul.@Beingrahuuls-MacBook-Air.local>
2024-07-20 12:30:40 +0530

    3nd

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended : true}));
 
   
 app.use(cors({
-  origin: 'https://task-client-eight.vercel.app/', // Specify the frontend origin
+  origin: 'https://task-client-eight.vercel.app', // Specify the frontend origin
   credentials: true
 }));
 
