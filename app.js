7266b73a824d405919152bd2903a3cb28eb35a45
Beingrahuul <beingrahuul.@Beingrahuuls-MacBook-Air.local>
2024-07-20 12:20:30 +0530

    first commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended : true}));
 
   
 app.use(cors({
-  origin: 'http://localhost:5173', // Specify the frontend origin
+  origin: 'https://task-client-eight.vercel.app/', // Specify the frontend origin
   credentials: true
 }));
 
